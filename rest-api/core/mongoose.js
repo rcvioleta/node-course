be0d7c9f42858376e1bae7082e6c18588b515d6d
Rogene Cris Violeta <rcvioleta13@gmail.com>
2020-02-22 08:58:42 +0800

    Updated mongoose.js

--- rest-api/core/mongoose.js
+++ rest-api/core/mongoose.js
@@ -1,42 +1,53 @@
 // Next Topic: 5. Data Validation and Sanitazion Part II
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const dsn = "mongodb://127.0.0.1:27017/ecommerce";
+const dsn = 'mongodb://127.0.0.1:27017/ecommerce';
 const options = {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
 };
 
 mongoose.connect(dsn, options);
 
-const User = mongoose.model("User", {
-  name: {
-    type: String
-  },
-  age: {
-    type: Number
-  },
-  email: {
-    type: String,
-    required: true,
-    validate: email => {
-      const regex = /\S+@\S+.\S+/;
-      if (!regex.test(email)) {
-        throw new Error("Email address is invalid!");
-      }
-    }
-  }
+const User = mongoose.model('User', {
+	name: {
+		type: String
+	},
+	age: {
+		type: Number
+	},
+	email: {
+		type: String,
+		required: true,
+		validate: (email) => {
+			const regex = /\S+@\S+.\S+/;
+			const errMsg = 'Email address is invalid!';
+			if (!regex.test(email)) {
+				throw new Error(errMsg);
+			}
+		}
+	},
+	password: {
+		type: String,
+		required: true,
+		trim: true,
+		minLength: 6,
+		validate: (password) => {
+			const errMsg = 'Password must not contain a word "password"!';
+			if (/password/i.test(password)) {
+				throw new Error(errMsg);
+			}
+		}
+	}
 });
 
 const newUser = new User({
-  name: "Super Admin",
-  age: "27",
-  email: "super-admin@test.com"
+	name: 'Super Admin',
+	age: '27',
+	email: 'super-admin@test.com',
+	password: 'newPassword'
 });
 
-newUser
-  .save()
-  .then(res => console.log(res))
-  .catch(e => console.log(e));
+newUser.save().then((res) => console.log(res)).catch((e) => console.log(e));
